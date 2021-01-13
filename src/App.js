e789f4a6ac05f3082fb9c92d4be8c162b187e265
JP Meyers <johnpmeyers20@yahoo.com>
2021-01-13 17:57:15 -0500

    added new state value for SearchBox, added row to App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState([]);
 
   const getMovieRequest = async () => {
     const url = 'http://www.omdbapi.com/?apikey=1f020500&s=star wars';
@@ -22,6 +25,13 @@ const App = () => {
 
   return (
     <div className="container-fluid fcc-omdb">
+      <div className="row">
+        <MovieListHeading heading='Movies' />
+        <SearchBox
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
